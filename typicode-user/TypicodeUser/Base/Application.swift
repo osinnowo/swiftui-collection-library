7ef60ef6e8fd268e80e2a3a7cecf07d2cb5d778b
osinnowo <liquidcoding2009@gmail.com>
2024-02-24 22:26:34 +0100

    Added ScenePhas

--- typicode-user/TypicodeUser/Base/Application.swift
+++ typicode-user/TypicodeUser/Base/Application.swift
@@ -9,12 +9,26 @@ import SwiftUI
 
 @main
 struct Application: App {
+    
+    @Environment(\.scenePhase) private var scenePhase
     let persistenceController = PersistenceController.shared
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+        .onChange(of: scenePhase) { _, phase in
+            switch phase {
+                case .active:
+                    break
+                case .background:
+                    break
+                case .inactive:
+                    break
+                default:
+                    break
+            }
         }
     }
 }
